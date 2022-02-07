d4acd67255c55304b64e5f3ac9ddca4e5aa8b2ac
SHARON <clubofsharon@gmail.com>
2022-02-07 14:53:23 +0530

    publish

--- app/src/main/java/com/techjays/country/MainActivity.kt
+++ app/src/main/java/com/techjays/country/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.techjays.country
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.techjays.techjayscountrycodepicker.app.activity.CountryPickerActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,8 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initBaseURL()
     }
-   private fun initBaseURL()
-   {
 
-   }
+    private fun initBaseURL() {
+        val intent = Intent(this, CountryPickerActivity::class.java)
+        intent.putExtra("base_url","http://34.224.39.147/api/portal/")
+        startActivity(intent)
+    }
 }
--- techjayscountrycodepicker/src/main/java/com/techjays/techjayscountrycodepicker/app/activity/CountryPickerActivity.kt
+++ techjayscountrycodepicker/src/main/java/com/techjays/techjayscountrycodepicker/app/activity/CountryPickerActivity.kt
@@ -10,9 +10,14 @@ class CountryPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_picker)
+        initView()
 
+    }
+
+    private fun initView() {
         val baseUrl = intent.getStringExtra("base_url")
         Toast.makeText(applicationContext, baseUrl, Toast.LENGTH_LONG).show()
-        CountryCodeLibrary.instance.baseUrl=baseUrl!!
+        CountryCodeLibrary.instance.baseUrl = baseUrl!!
+
     }
 }
